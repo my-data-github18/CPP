3a5e7c610099a790b0e5f52caf9e56a9f50daaa3
AkashBarbhai <akashbarbhai101@gmail.com>
2023-09-26 18:52:49 +0530

    Assignmnet 2 Q1 uploaded

--- Assignment4/Assign4Q3.cpp
+++ Assignment4/Assign4Q3.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+using namespace std;
 
 class matrix
 {
     int rows;
     int column;
+    int **arr;
 
 public:
     matrix()
     {
         this->rows = 3, this->column = 3;
-        int *arr[rows];
-        for(int i=0;i<3;i++){
-            arr[i]= new int[3];
+        arr = new int*[rows];
+        for (int i = 0; i < 3; i++)
+        {
+            arr[i] = new int[3];
         }
     }
     matrix(int rows, int column)
@@ -19,14 +22,53 @@ public:
 
         this->rows = rows, this->column = column;
 
-        int *arr[rows];
-        for(int i=0;i<rows;i++){
-            arr[i]= new int[column];
-        }
+               arr=new int*[rows];
 
+        for (int i = 0; i < rows; i++)
+        {
+            arr[i] = new int[column];
+        }
+    }
+    void accept()
+    {
+        cout << "enter the values in matrix:" << endl;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+    }
+    void display()
+    {
+        cout << " The matrix is :" << endl;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                cout << arr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    
+    ~matrix()
+    {
+        for(int i =0; i<rows;i++)
+        delete[] arr[i];
+
+        delete[] arr;
     }
 };
-int main(){
+int main()
+{
     matrix m1;
+    m1.accept();
+    m1.display();
+    matrix m2;
+    m2.accept();
+    m2.display();
+
     
 }
